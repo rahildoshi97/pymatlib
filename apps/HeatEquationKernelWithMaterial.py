883710868bce35ca100b718ac13d163e09c769b9
Rahil Doshi <rahil.doshi@fau.de>
2025-02-27 17:04:19 +0100

    Update HeatEquationWithMaterial to use the new yaml config

--- apps/HeatEquationKernelWithMaterial.py
+++ apps/HeatEquationKernelWithMaterial.py
@@ -1,14 +1,11 @@
 import sympy as sp
 import pystencils as ps
-# from pystencils_walberla import CodeGeneration, generate_sweep
-# script_dir = os.path.dirname(__file__)
-# walberla_dir = os.path.join(script_dir, '..', '..', '..')
-# sys.path.append(walberla_dir)
+from importlib.resources import files
 from pystencilssfg import SourceFileGenerator
 from sfg_walberla import Sweep
-from pymatlib.data.alloys.SS316L import SS316L
 from pymatlib.core.assignment_converter import assignment_converter
 from pymatlib.core.interpolators import DoubleLookupArrayContainer
+from pymatlib.core.yaml_parser import create_alloy_from_yaml
 
 with SourceFileGenerator() as sfg:
     data_type = "float64"  # if ctx.double_accuracy else "float32"
@@ -24,21 +21,14 @@ with SourceFileGenerator() as sfg:
     heat_pde_discretized = discretize(heat_pde)
     heat_pde_discretized = heat_pde_discretized.args[1] + heat_pde_discretized.args[0].simplify()
 
-    mat = SS316L.create_SS316L(u.center())
+    yaml_path = files('pymatlib.data.alloys.SS316L').joinpath('SS304L.yaml')
+    mat = create_alloy_from_yaml(yaml_path, u.center())
     # arr_container = DoubleLookupArrayContainer("SS316L", mat.temperature_array, mat.energy_density_array)
     arr_container = DoubleLookupArrayContainer.from_material("SS316L", mat)
     sfg.generate(arr_container)
 
     # Convert assignments to pystencils format
-    print("Print statements")
-    print(mat.thermal_diffusivity)
-    print(mat.thermal_diffusivity.expr)
-    print(mat.thermal_diffusivity.assignments)
     subexp, subs = assignment_converter(mat.thermal_diffusivity.assignments)
-    print(f"subexp\n{subexp}")
-    print(f"subs\n{subs}")
-    # subexp_ed, _ = assignment_converter(mat.energy_density.assignments)
-    # print(f"subexp_ed =\n{subexp_ed}")
 
     subexp.append(ps.Assignment(thermal_diffusivity, mat.thermal_diffusivity.expr))
 
@@ -48,14 +38,6 @@ with SourceFileGenerator() as sfg:
             ps.Assignment(u_tmp.center(), heat_pde_discretized),
             ps.Assignment(thermal_diffusivity_out.center(), thermal_diffusivity)
         ])
-
-    # ac = ac.new_with_substitutions(subs)
-
-    # ac = ps.simp.simplifications.add_subexpressions_for_divisions(ac)
-    # ac = ps.simp.simplifications.add_subexpressions_for_field_reads(ac)
-    # ac = ps.simp.sympy_cse(ac)
-
-    print(f"ac\n{ac}")
 
     # generate_sweep(ctx, 'HeatEquationKernelWithMaterial', ac, varying_parameters=((data_type, str(thermal_diffusivity)),))
     sweep = Sweep("HeatEquationKernelWithMaterial", ac)
