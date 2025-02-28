fb69f5eb99c5946c5a3016ceb0a66015086d97c1
Rahil Doshi <rahil.doshi@fau.de>
2025-02-28 17:57:49 +0100

    Update TestArrayContainer.py

--- src/pymatlib/core/cpp/TestArrayContainer.py
+++ src/pymatlib/core/cpp/TestArrayContainer.py
@@ -11,8 +11,11 @@ with SourceFileGenerator() as sfg:
     arr_container = DoubleLookupArrayContainer.from_material("SS316L", mat)
     sfg.generate(arr_container)'''
 
+    T = np.array([3243.15, 3253.15, 3263.15, 3273.15], dtype=np.float64)
+    E = np.array([1.68e10, 1.69e10, 1.70e10, 1.71e10], dtype=np.float64)
 
-    custom_container = InterpolationArrayContainer("DoubleLookupTests",
-                                       np.array([3243.15, 3253.15, 3263.15, 3273.15], dtype=np.float64),
-                                       np.array([1.68e10, 1.69e10, 1.70e10, 1.71e10], dtype=np.float64))
+    custom_container = InterpolationArrayContainer("DoubleLookupTests", np.flip(T), np.flip(E))
+    sfg.generate(custom_container)
+
+    custom_container = InterpolationArrayContainer("BinarySearchTests", T, E)
     sfg.generate(custom_container)
