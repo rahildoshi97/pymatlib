eeae9c50e4a986f18fd0a45b90efa1c42a283eac
Rahil Doshi <rahil.doshi@fau.de>
2025-03-24 13:59:19 +0100

    Generalize cpp interpolation code to use x and y arrays

--- src/pymatlib/core/cpp/include/pymatlib_interpolators/interpolate_double_lookup_cpp.h
+++ src/pymatlib/core/cpp/include/pymatlib_interpolators/interpolate_double_lookup_cpp.h
@@ -5,36 +5,36 @@
 
 template<typename ArrayContainer>
 double interpolate_double_lookup_cpp(
-    double E_target,
+    double y_target,
     const ArrayContainer& arrs) {
 
     // Handle boundary cases
-    if (E_target <= arrs.E_neq[0]) return arrs.T_eq[0];
-    if (E_target >= arrs.E_neq.back()) return arrs.T_eq.back();
+    if (y_target <= arrs.y_neq[0]) return arrs.x_eq[0];
+    if (y_target >= arrs.y_neq.back()) return arrs.x_eq.back();
 
     // Calculate index in equidistant grid
-    int idx_E_eq = static_cast<int>((E_target - arrs.E_eq[0]) * arrs.inv_delta_E_eq);
-    idx_E_eq = std::min(idx_E_eq, static_cast<int>(arrs.idx_map.size() - 1));
+    int idx_y_eq = static_cast<int>((y_target - arrs.y_eq[0]) * arrs.inv_delta_y_eq);
+    idx_y_eq = std::min(idx_y_eq, static_cast<int>(arrs.idx_map.size() - 1));
 
     // Get index from mapping
-    int idx_E_neq = arrs.idx_map[idx_E_eq];
+    int idx_y_neq = arrs.idx_map[idx_y_eq];
 
     // Make sure we don't go out of bounds
-    idx_E_neq = std::min(idx_E_neq, static_cast<int>(arrs.E_neq.size() - 2));
+    idx_y_neq = std::min(idx_y_neq, static_cast<int>(arrs.y_neq.size() - 2));
 
     // Adjust index if needed
-    /*if (arrs.E_neq[idx_E_neq + 1] < E_target) {
-        ++idx_E_neq;
+    /*if (arrs.y_neq[idx_y_neq + 1] < y_target) {
+        ++idx_y_neq;
     }*/
-    idx_E_neq += arrs.E_neq[idx_E_neq + 1] < E_target;
+    idx_y_neq += arrs.y_neq[idx_y_neq + 1] < y_target;
 
     // Get interpolation points
-    const double E1 = arrs.E_neq[idx_E_neq];
-    const double E2 = arrs.E_neq[idx_E_neq + 1];
-    const double T1 = arrs.T_eq[idx_E_neq];
-    const double T2 = arrs.T_eq[idx_E_neq + 1];
+    const double y1 = arrs.y_neq[idx_y_neq];
+    const double y2 = arrs.y_neq[idx_y_neq + 1];
+    const double x1 = arrs.x_eq[idx_y_neq];
+    const double x2 = arrs.x_eq[idx_y_neq + 1];
 
     // Linear interpolation
-    const double slope = (T2 - T1) / (E2 - E1);
-    return T1 + slope * (E_target - E1);
+    const double slope = (x2 - x1) / (y2 - y1);
+    return x1 + slope * (y_target - y1);
 }
